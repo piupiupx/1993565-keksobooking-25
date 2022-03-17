873433bf8ab06b8c98f2b09c0b00430ae32548b4
piupiupx <akikokuroki1@gmail.com>
2022-03-17 18:41:26 +0300

    внесены доработки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandom(min, max) {
-  if (min > 0 && max > 0) {
+  if (min >= 0 && max >= 0) {
     if (min > max) {
-      c = max;
+      const c = max;
       max = min;
       min = c;
     }
@@ -9,36 +9,31 @@ function getRandom(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
   }
-  return "Числа должны быть натуральными";
+  return 'Числа должны быть натуральными';
 }
 
 function getDot(min, max) {
   // рандомное число с плавающей точкой
   if (min > 0 && max > 0) {
     if (min > max) {
-      c = max;
+      const c = max;
       max = min;
       min = c;
     }
     return Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
   }
-  return "Числа должны быть натуральными";
+  return 'Числа должны быть натуральными';
 }
 
-function getRandomIndx(min, max) {
-  // функция возвращает рандомный индекс включая 0
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-function getRandLengthArray(nameArr, countEl) {
+function getRandLengthArray(nameArr) {
   // первый параметр - массив, второй параметр - номер последнего элемента массива, функция создает массив случайной длины из указанного массива
-  let maxLength = nameArr.length;
-  let lengthOfArray = getRandom(1, maxLength);
-  let arr = [];
+  const countEl = nameArr.length - 1;
+  const maxLength = nameArr.length;
+  const lengthOfArray = getRandom(1, maxLength);
+  const arr = [];
   for (let i = 0; i < lengthOfArray; i++) {
-    let indexEl = getRandomIndx(0, countEl);
-    let el = nameArr[indexEl];
+    const indexEl = getRandom(0, countEl);
+    const el = nameArr[indexEl];
 
     if (!arr.includes(el)) {
       arr.push(el);
@@ -48,56 +43,59 @@ function getRandLengthArray(nameArr, countEl) {
 }
 
 const photo = [
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const checkinout = ["12:00", "13:00", "14:00"];
+const checkinout = ['12:00', '13:00', '14:00'];
 
 const feat = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner",
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
 ];
-const typeArr = ["palace", "flat", "house", "bungalow", "hotel"];
-let pict = "img/avatars/user{{xx}}.png";
+const typeArr = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const pict = 'img/avatars/user{{xx}}.png';
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomIndx(0, elements.length - 1)];
-};
+const getRandomArrayElement = (elements) =>
+  elements[getRandom(0, elements.length - 1)];
 
+// eslint-disable-next-line no-unused-vars
 const createAppart = () => {
   const location = {
     lat: getDot(35.65, 35.7),
     lng: getDot(139.7, 139.8),
   };
-  const ind4 = getRandomIndx(0, 4); // рандомно ищет индекс для типа жилища
-  let typeApp = typeArr[ind4]; // рандомный тип жилища
+  const ind4 = getRandom(0, 4); // рандомно ищет индекс для типа жилища
+  const typeApp = typeArr[ind4]; // рандомный тип жилища
   const rand10 = getRandom(1, 10);
-  let newstr = pict.replace("{{xx}}", rand10 < 10 ? `0${rand10}` : rand10); // заменяет в строке avatar {{хх}} на число от 01 до 10
+  const newstr = pict.replace('{{xx}}', rand10 < 10 ? `0${rand10}` : rand10); // заменяет в строке avatar {{хх}} на число от 01 до 10
 
   return {
+    location,
     author: {
       avatar: newstr,
     },
     offer: {
-      title: "Временное описание",
+      title: 'Временное описание',
       guests: getRandom(1, 10),
       address: `${location.lat}, ${location.lng}`,
       prise: getRandom(1, 500),
       type: typeApp,
       rooms: getRandom(1, 10),
+      // eslint-disable-next-line no-dupe-keys
       guests: getRandom(1, 10),
       checkin: getRandomArrayElement(checkinout),
       checkout: getRandomArrayElement(checkinout),
-      features: getRandLengthArray(feat, 5),
-      description: "Временное описание",
-      photos: getRandLengthArray(photo, 2),
+      features: getRandLengthArray(feat),
+      description: 'Временное описание',
+      photos: getRandLengthArray(photo),
     },
   };
 };
+// eslint-disable-next-line no-unused-vars
 const similarAppart = Array.from({ length: 10 }, createAppart); // создает 10 объектов с рандомными значениями
 console.log(similarAppart);
