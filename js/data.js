b6de00670c7587ef1f47785371710d6718e1d6ff
piupiupx <akikokuroki1@gmail.com>
2022-03-18 18:44:12 +0300

    файл main.js разбит на модули

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const photo = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const checkinout = ['12:00', '13:00', '14:00'];
+
+const feat = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const typeArr = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const pict = 'img/avatars/user{{xx}}.png';
---
+++ js/util.js
@@ -0,0 +1,45 @@
+function getRandom(min, max) {
+  if (min >= 0 && max >= 0) {
+    if (min > max) {
+      const c = max;
+      max = min;
+      min = c;
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  return 'Числа должны быть натуральными';
+}
+
+function getDot(min, max) {
+  // рандомное число с плавающей точкой
+  if (min > 0 && max > 0) {
+    if (min > max) {
+      const c = max;
+      max = min;
+      min = c;
+    }
+    return Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
+  }
+  return 'Числа должны быть натуральными';
+}
+
+function getRandLengthArray(nameArr) {
+  // первый параметр - массив, второй параметр - номер последнего элемента массива, функция создает массив случайной длины из указанного массива
+  const countEl = nameArr.length - 1;
+  const maxLength = nameArr.length;
+  const lengthOfArray = getRandom(1, maxLength);
+  const arr = [];
+  for (let i = 0; i < lengthOfArray; i++) {
+    const indexEl = getRandom(0, countEl);
+    const el = nameArr[indexEl];
+
+    if (!arr.includes(el)) {
+      arr.push(el);
+    }
+  }
+  return arr;
+}
+const getRandomArrayElement = (elements) =>
+  elements[getRandom(0, elements.length - 1)];
