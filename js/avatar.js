2e444c0d02502298864c8ab7b987c97486f3eefc
piupiupx <akikokuroki1@gmail.com>
2022-04-17 23:38:49 +0300

    12.10 Фото на память fix

--- js/avatar.js
+++ js/avatar.js
@@ -4,11 +4,10 @@ const fileChooserAvatar = document.querySelector(
 const preview = document.querySelector('.ad-form-header__preview');
 const img = preview.getElementsByTagName('img');
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const file = fileChooserAvatar.files[0];
+const fileName = file.name.toLowerCase();
+
 fileChooserAvatar.addEventListener('change', () => {
-  const file = fileChooserAvatar.files[0];
-
-  const fileName = file.name.toLowerCase();
-
   // eslint-disable-next-line arrow-body-style
   const matches = FILE_TYPES.some((it) => {
     return fileName.endsWith(it);
@@ -24,9 +23,6 @@ const fileChooserAdvertise = document.querySelector(
 const previewAdvertise = document.querySelector('.ad-form__photo');
 
 fileChooserAdvertise.addEventListener('change', () => {
-  const file = fileChooserAdvertise.files[0];
-  const fileName = file.name.toLowerCase();
-
   // eslint-disable-next-line arrow-body-style
   const matches = FILE_TYPES.some((it) => {
     return fileName.endsWith(it);
@@ -36,8 +32,8 @@ fileChooserAdvertise.addEventListener('change', () => {
       'afterbegin',
       '<img src="img/muffin-grey.svg" alt="Фото жилья" width="70" height="70" />'
     );
-    const imgadv = previewAdvertise.getElementsByTagName('img');
+    const imgAdv = previewAdvertise.getElementsByTagName('img');
 
-    imgadv[0].src = URL.createObjectURL(file);
+    imgAdv[0].src = URL.createObjectURL(file);
   }
 });
