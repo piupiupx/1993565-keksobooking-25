5e2710aae640c9c2b2f7a43025fb13af067c083d
piupiupx <66266861+piupiupx@users.noreply.github.com>
2022-04-18 11:57:33 +0300

    Update avatar.js
    fileName внесен в локальные области функций (из глобальной)

--- js/avatar.js
+++ js/avatar.js
@@ -5,11 +5,13 @@ const preview = document.querySelector('.ad-form-header__preview');
 const img = preview.getElementsByTagName('img');
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const file = fileChooserAvatar.files[0];
-const fileName = file.name.toLowerCase();
 
 fileChooserAvatar.addEventListener('change', () => {
+  const fileName = file.name.toLowerCase();
+
   // eslint-disable-next-line arrow-body-style
   const matches = FILE_TYPES.some((it) => {
+
     return fileName.endsWith(it);
   });
   if (matches) {
@@ -25,6 +27,8 @@ const previewAdvertise = document.querySelector('.ad-form__photo');
 fileChooserAdvertise.addEventListener('change', () => {
   // eslint-disable-next-line arrow-body-style
   const matches = FILE_TYPES.some((it) => {
+  const fileName = file.name.toLowerCase();
+
     return fileName.endsWith(it);
   });
   if (matches) {
